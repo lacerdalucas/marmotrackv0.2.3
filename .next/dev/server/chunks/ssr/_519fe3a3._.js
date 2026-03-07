e33d53c86a09fe84c7e994e4335352beaaa2d657
lacerdalucas <lacerda.lucas@gmail.com>
2026-03-06 22:03:33 -0300

    fix: atualiza export do proxy.ts para compatibilidade com Next.js 16.1

--- .next/dev/server/chunks/ssr/_519fe3a3._.js
+++ .next/dev/server/chunks/ssr/_519fe3a3._.js
@@ -75,7 +75,7 @@ function getRedirectRouteByRole(role) {
 "[project]/app/(auth)/login/actions.ts [app-rsc] (ecmascript)", ((__turbopack_context__) => {
 "use strict";
 
-/* __next_internal_action_entry_do_not_use__ [{"40142f58552fae12e2fc6701ec52bbec75edc1f090":"loginAction"},"",""] */ __turbopack_context__.s([
+/* __next_internal_action_entry_do_not_use__ [{"40b1d65094deaecf9214c93016152615548be9ba2d":"loginAction"},"",""] */ __turbopack_context__.s([
     "loginAction",
     ()=>loginAction
 ]);
@@ -149,7 +149,7 @@ async function loginAction(formData) {
 (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$build$2f$webpack$2f$loaders$2f$next$2d$flight$2d$loader$2f$action$2d$validate$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["ensureServerEntryExports"])([
     loginAction
 ]);
-(0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$build$2f$webpack$2f$loaders$2f$next$2d$flight$2d$loader$2f$server$2d$reference$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["registerServerReference"])(loginAction, "40142f58552fae12e2fc6701ec52bbec75edc1f090", null);
+(0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$build$2f$webpack$2f$loaders$2f$next$2d$flight$2d$loader$2f$server$2d$reference$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["registerServerReference"])(loginAction, "40b1d65094deaecf9214c93016152615548be9ba2d", null);
 }),
 "[project]/.next-internal/server/app/(auth)/login/page/actions.js { ACTIONS_MODULE0 => \"[project]/app/(auth)/login/actions.ts [app-rsc] (ecmascript)\" } [app-rsc] (server actions loader, ecmascript) <locals>", ((__turbopack_context__) => {
 "use strict";
@@ -162,7 +162,7 @@ var __TURBOPACK__imported__module__$5b$project$5d2f$app$2f28$auth$292f$login$2f$
 "use strict";
 
 __turbopack_context__.s([
-    "40142f58552fae12e2fc6701ec52bbec75edc1f090",
+    "40b1d65094deaecf9214c93016152615548be9ba2d",
     ()=>__TURBOPACK__imported__module__$5b$project$5d2f$app$2f28$auth$292f$login$2f$actions$2e$ts__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["loginAction"]
 ]);
 var __TURBOPACK__imported__module__$5b$project$5d2f2e$next$2d$internal$2f$server$2f$app$2f28$auth$292f$login$2f$page$2f$actions$2e$js__$7b$__ACTIONS_MODULE0__$3d3e$__$225b$project$5d2f$app$2f28$auth$292f$login$2f$actions$2e$ts__$5b$app$2d$rsc$5d$__$28$ecmascript$2922$__$7d$__$5b$app$2d$rsc$5d$__$28$server__actions__loader$2c$__ecmascript$29$__$3c$locals$3e$__ = __turbopack_context__.i('[project]/.next-internal/server/app/(auth)/login/page/actions.js { ACTIONS_MODULE0 => "[project]/app/(auth)/login/actions.ts [app-rsc] (ecmascript)" } [app-rsc] (server actions loader, ecmascript) <locals>');
--- .next/dev/static/chunks/app_(auth)_login_page_tsx_893e5ca7._.js
+++ .next/dev/static/chunks/app_(auth)_login_page_tsx_893e5ca7._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_92e77777._.js"
+  "static/chunks/_9ed56cf2._.js"
 ],
     source: "dynamic"
 });
